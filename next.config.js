628a0ea051a7dc4772092d9b509febc2d5c60b28
David Flindall <david@curiousways.com>
2022-10-30 19:03:09 +0000

    add GA ID and site URL

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   env: {
-    SITE_URL: "site url",
+    SITE_URL: "https://www.studionotion.com",
     SITE_TITLE:
       "Studio Notion - unrivalled access for brands to new music and Generation Z",
-    GOOGLE_ANALYTICS_ID: "yourgaid",
+    GOOGLE_ANALYTICS_ID: "UA-17566718-10",
     FATHOM_ANALYTICS_ID: "yourfathomanalyticsid",
   },
 };
